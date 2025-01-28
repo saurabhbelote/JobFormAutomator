0e8260eb0a50cdbffd11461934224c8f1e6d74f9
Mahaprasad Prusty <mahaprasadprusty72@gmail.com>
2025-01-28 23:10:16 +0530

    added Celibi

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,6 +1,7 @@
 // import Artboard from '@/components/center/Artboard'
 import LeftSidebar from "@/components/left/LeftSidebar";
 import Rightsidebar from '@/components/right/Rightsidebar';
+import Celibi from "@/components/resume_templates/Celibi";
 
 const BuilderArea = () => {
   return (
@@ -8,6 +9,10 @@ const BuilderArea = () => {
       {/* Left Sidebar */}
       <div className="border-4 border-black w-3/12">
         <LeftSidebar />
+      </div>
+
+      <div className="border-4 border-blue-50 w-6/12">
+        <Celibi/>
       </div>
 
       {/* Right Sidebar */}
