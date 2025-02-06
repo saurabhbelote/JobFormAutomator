7022fad0ac1e05e72ed86bd99bd26fa1a3c95233
kumar1397 <sahuiswar007@outlook.com>
2025-02-07 01:42:44 +0530

    working on hero page

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <section className="bg-gradient-to-b from-[#11011E] via-[#35013e] to-[#11011E] bg-[#11011E] py-16 pt-24 px-6 md:px-16 lg:px-20 text-white text-center">
+            hello
+      </section>
+    );
+  }
